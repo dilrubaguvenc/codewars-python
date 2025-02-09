981a5ef8d9ce09bf8ad095b2738f565ca0ab2e98
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2025-02-08 21:55:24 -0500

    added the solution

---
+++ multiply-the-number.py
@@ -0,0 +1,19 @@
+# Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+#   3 -->    15  (  3 * 5¹)
+#  10 -->   250  ( 10 * 5²)
+# 200 --> 25000  (200 * 5³)
+#   0 -->     0  (  0 * 5¹)
+#  -3 -->   -15  ( -3 * 5¹)
+
+
+# my solution 
+
+def multiply(n):
+    if n == 0:
+        return 0
+    num_digits = len(str(abs(n)))
+    
+    result = n * (5 ** num_digits)
+    
+    return result
